926850efd345d261fa10e3baf9074fee15747462
Tony Garrett <tony.garrett@gmail.com>
2023-10-21 09:38:04 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+
+namespace InventoryPro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProcessFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
